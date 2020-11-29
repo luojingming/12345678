2a577f986eaf2891e60b3ee5ee84b31a7251235a
a2133246313 <a2133246313@163.com>
2020-11-29 20:29:11 +0800

    ex-1

---
+++ ex-1/demo.h
@@ -0,0 +1,20 @@
+#ifndef demo_h
+#define demo_h
+#include <stdio.h>
+#include <stdlib.h>
+typedef int dataType;
+typedef struct node {
+	dataType data;
+	struct node *next;
+} LinkList;
+
+LinkList* CreateList();
+int Size(LinkList *list);
+void Insert(LinkList *list,int k, dataType x);
+void Delete(LinkList *list, int k);
+int Empty(LinkList *list);
+dataType GetData(LinkList *list, int k);
+LinkList* Find(LinkList *list, dataType x);
+void Print(LinkList *list);
+void ClearList (LinkList *list);
+#endif
