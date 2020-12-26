3a81161e4d2cd8b16ee2ea8e070d867646e507d4
a2133246313 <a2133246313@163.com>
2020-12-26 17:02:40 +0800

    ex-5

---
+++ ex-5/shu.h
@@ -0,0 +1,18 @@
+#ifndef shu_h
+#define shu_h
+#include <stdio.h>
+#include <malloc.h>
+#define MaxSize 100
+
+typedef char dataType;
+typedef struct TreeNode {
+    dataType data;
+    struct TreeNode *left, *right;
+} TreeNode;
+
+void CreateTree(TreeNode *t,dataType x);
+void PreOrder(TreeNode *t);
+void InOrder(TreeNode *t);
+void PostOrder(TreeNode *t);
+void LevelOrder(TreeNode *t);
+#endif
